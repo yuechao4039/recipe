9844e799e66845662d36f29d45dc865eee348718
yuechao <yuechao4039@sina.com>
2018-05-04 21:25:51 +0800

    xml 1

--- src/test/java/com/sndj/recipe/innerclssss/TestClass.java
+++ src/test/java/com/sndj/recipe/innerclssss/TestClass.java
@@ -3,59 +3,94 @@ package com.sndj.recipe.innerclssss;
 import javafx.scene.Parent;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.*;
 
 /**
  * @author yuechao 2018/4/20
  */
 
 public class TestClass {
+
+
     @Test
-    public void test() throws Exception {
-        // generic type to raw type
-        Box<String> stringBox = new Box<>();
-        Box rawBox = stringBox;               // OK
-        rawBox.set(8);  // warning: unchecked invocation to set(T)
+    public void testParameterizedType() {
+        Field f = null;
+        try {
+            Field[] fields = ParameterizedTypeBean.class.getDeclaredFields();
+            // 打印出所有的 Field 的 Type 是否属于 ParameterizedType
+            for (int i = 0; i < fields.length; i++) {
+                f = fields[i];
+                System.out.println((f.getName()
+                        + " getGenericType() instanceof ParameterizedType "
+                        + (f.getGenericType() instanceof ParameterizedType)));
+            }
+            System.out.println("=========================");
+            Arrays.asList(ParameterizedTypeBean.class.getDeclaredFields())
+                    .stream()
+//                    .filter(x -> x.getName().equals("str"))
+                    .forEach(x -> {
+                x.getGenericType().toString();
+                System.out.println(x.getGenericType());
+            });
+            System.out.println("=========================");
+            getParameterizedTypeMes("map");
+            System.out.println("=========================");
+            getParameterizedTypeMes("entry");
+            System.out.println("=========================");
+            getParameterizedTypeMes("holder");
 
-        // raw type to generic type
-        Box rawBox1 = new Box();           // rawBox is a raw type of Box<T>
-        Box<Integer> intBox = rawBox1;     // warning: unchecked conversion
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+    }
 
-        List<String> list = new ArrayList<>();
-        list.add("aaa");
-        List rawList = list;
-        rawList.add(12);
-        rawList.forEach(System.out::println);
+    private static void getParameterizedTypeMes(String fieldName) throws Exception {
+        Field f;
+        f = ParameterizedTypeBean.class.getDeclaredField(fieldName);
+        f.setAccessible(true);
+        System.out.println(f.getGenericType());
+        if (f.getGenericType() instanceof ParameterizedType) {
+            ParameterizedType pType = (ParameterizedType) f.getGenericType();
+            System.out.println(pType.getRawType());
+            for (Type type : pType.getActualTypeArguments()) {
+                System.out.println(type);
+            }
+            System.out.println(pType.getOwnerType()); // null
+        }
     }
 }
 
-class Box<T> {
-    // T stands for "Type"
-    private T t;
+class ParameterizedTypeBean {
+    // 下面的 field 的 Type 属于 ParameterizedType
+    Map<String, User> map;
+    Set<String> set1;
+    Class<?> clz;
+    Holder<String> holder;
+    List<String> list;
 
-    public void set(T t) {
-        this.t = t;
-    }
+    // Map<String,Person> map 这个 ParameterizedType 的 getOwnerType() 为 null，
+    // 而 Map.Entry<String, String> entry 的 getOwnerType() 为 Map 所属于的 Type。
+    Map.Entry<String, String> entry;
 
-    public T get() {
-        return t;
-    }
-}
+    // 下面的 field 的 Type 不属于ParameterizedType
+    String str;
+    Integer i;
+    Set set;
+    List aList;
 
-class WildcardFixed{
-    void foo(List<?> i){
-        fooHelper(i);
-    }
-
-    // Helper method created so that the wildcard can be captured
-    // through type inference.
-    private <T> void fooHelper(List<T> l){
-        l.set(0, l.get(0)); // 传入参数也为 T，编译器推断为 CAP#1
+    static class Holder<V> {
     }
 }
+
+class User {
+
+}
+
+
 
 
 
